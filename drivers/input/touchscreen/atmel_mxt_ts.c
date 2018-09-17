548d74b8ae8e916bf926a8af6dae4ac1b70f4797
Svyatoslav Ryhel <clamor95@gmail.com>
2018-09-17 09:23:07 +0300

    atmel_mxt_ts: add delay between initialization and irq request
    Hopefully this should fix occasional touchscreen failures on Asus Transformer Prime.
    
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -2560,6 +2560,9 @@ static int __devinit mxt_probe(struct i2c_client *client,
 	if (error)
 		goto err_free_object;
 
+	/* Need a delay for stabilization */
+	msleep(150);
+
 	error = request_threaded_irq(client->irq, NULL, mxt_interrupt,
 			pdata->irqflags, client->dev.driver->name, data);
 	if (error) {
